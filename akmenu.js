882e1f4140667ef0db59c45155bfe94c1c2b710d
Andrii Kos <andrii.kostrytsia@gmail.com>
2019-03-04 23:24:46 +0100

    1

---
+++ akmenu.js
@@ -0,0 +1,5 @@
+document.write("<li><a href=\"photography.html\">Photography<\/a><\/li>");
+document.write("<li><a href=\"travel.html\">Travel<\/a><\/li>");
+document.write("<li><a href=\"fashion.html\">Fashion<\/a><\/li>");
+document.write("<li><a href=\"about.html\">About<\/a><\/li>");
+document.write("<li><a href=\"contact.html\">Contact<\/a><\/li>");
